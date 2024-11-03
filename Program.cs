06f680db922edf23331f34e7f484e38d52f04c34
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-11-03 08:14:50 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//*****************************************************************************
+//** 796. Rotate String     leetcode                                         **
+//*****************************************************************************
+
+
+bool rotateString(char* s, char* goal) {
+    int lenS = strlen(s);
+    int lenGoal = strlen(goal);
+
+    // If the lengths of s and goal are not equal, goal cannot be a rotation of s
+    if (lenS != lenGoal) {
+        return false;
+    }
+
+    // Create a new string that is s concatenated with itself
+    char doubleS[2 * lenS + 1];
+    strcpy(doubleS, s);
+    strcat(doubleS, s);
+
+    // Check if goal is a substring of doubleS
+    return strstr(doubleS, goal) != NULL;
+}
